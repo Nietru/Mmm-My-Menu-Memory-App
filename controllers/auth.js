85520407171c44b401a8e7a457052921f8fcf53c
Tifannie Truman <nietru143@gmail.com>
2023-04-17 19:02:30 -0400

    login js and auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,28 @@
-// login authentification route via passwordjs strategy
-const express = require("express");
-const router = express.Router();
-const passport = require("../passport/strategies");
+// login authentification route via passportjs strategy
+// const express = require("express");
+// const router = express.Router();
+// const passport = require("../passport/strategies");
 
-router.get("/login", function (req, res, next) {
-  res.render("login");
-});
+// router.get("/login", function (req, res, next) {
+//   res.render("login");
+// });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
+// router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//   })
+// );
 
-module.exports = router;
+// module.exports = router;
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
