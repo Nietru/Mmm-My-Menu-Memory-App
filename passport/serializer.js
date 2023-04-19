cd0ad70106f701fae5b3733321f75b345dec8656
Tifannie Truman <nietru143@gmail.com>
2023-04-19 19:43:32 -0400

    working with bobby on passport

--- passport/serializer.js
+++ passport/serializer.js
@@ -2,13 +2,15 @@ var User = require("../models/user");
 
 // Tells sequelize how to use the User in a request/response.
 module.exports = function (passport) {
-  passport.serializeUser(function (user, done) {
-    done(null, { id: user.id, username: user.name });
+  passport.serializeUser(function (email, done) {
+    User.findOne({ where: { email: email } }).then(function (user) {
+      done(null, { id: user.id, username: user.name });
+    });
   });
   // convert it back to what it was...
-  passport.deserializeUser(function ({ id }, done) {
-    console.log(id);
-    User.findByPk(id).then(function (user) {
+  passport.deserializeUser(function (_user, done) {
+    console.log("USER", _user);
+    User.findByPk(_user.id).then(function (user) {
       if (user) {
         done(null, { id: user.id, username: user.name });
       } else {
