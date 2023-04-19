d499d8430377217fd66b563526eacab171515511
Tifannie Truman <nietru143@gmail.com>
2023-04-19 18:33:07 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
--- models/index.js
+++ models/index.js
@@ -1,6 +1,10 @@
 const Recipe = require("./recipe");
 const User = require("./user");
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, { dialect: "mysql" });
+}
+
 User.hasMany(Recipe, {
   foreignKey: "user_id",
 });
@@ -9,8 +13,7 @@ Recipe.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-
 module.exports = {
-    User,
-    Recipe,
+  User,
+  Recipe,
 };
