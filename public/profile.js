99fc3ac0269bfd61a67d32d29dadd862b4909a27
Tifannie Truman <nietru143@gmail.com>
2023-04-19 07:40:42 -0400

    linked handlebars to their respective js files correctly

--- public/profile.js
+++ public/profile.js
@@ -1,36 +1,28 @@
-const deleteRecipeHandler = async (event) =>{
+const deleteRecipeHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
-if (event.target.hasAttribute('data-id')){
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/recipe/${id}`,{
-        method:'DELETE'
+    const response = await fetch(`/api/recipe/${id}`, {
+      method: "DELETE",
     });
 
-    if(response.ok){
-        document.location.replace('/profile')
+    if (response.ok) {
+      document.location.replace("/profile");
     } else {
-        alert('Failed to delete recipe')
+      alert("Failed to delete recipe");
     }
-}
+  }
+};
 
+const editRecipeHandler = (event) => {
+  console.log("edit recipe clicked");
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    document.location.replace(`/editrecipe/${id}`);
+  }
+};
 
-}
-
-
-
-
-const editRecipeHandler = (event) =>{
-    console.log('edit recipe clicked')
-if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-    document.location.replace(`/editrecipe/${id}`)
-}
-
-
-}
-
-
-
-document.querySelector('#delete').addEventListener('click',deleteRecipeHandler)
-document.querySelector('#edit').addEventListener('click',editRecipeHandler)
+document
+  .querySelector("#delete")
+  .addEventListener("click", deleteRecipeHandler);
+document.querySelector("#edit").addEventListener("click", editRecipeHandler);
